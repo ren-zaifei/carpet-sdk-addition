5a7cbb78edb06c54d7a2d01cf241e5ebc0d1dba7
人哉肥 <chuen6@qq.com>
2026-01-24 06:18:25 +0800

    2026.1.24：初始化项目

---
+++ src/main/java/com/renzaifei/carpetsdkaddition/CarpetSDKAddition.java
@@ -0,0 +1,17 @@
+package com.renzaifei.carpetsdkaddition;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CarpetSDKAddition implements ModInitializer {
+	public static final String MOD_ID = "carpet-sdk-addition";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
